ddc90ff69bea24a7c9661291a5188e5bffb247fb
Tuan Anh <anh.pngt@gmail.com>
2021-12-17 19:22:58 +0700

    Remove obsolete run.bash

--- content/posts/unleash-self-host/examples/run.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/manabie-com/manabie-com.github.io
-cd manabie-com.github.io/content/posts/unleash-self-host/examples
-
-minikube start
-helm upgrade --install unleash ./ -f values.yaml
